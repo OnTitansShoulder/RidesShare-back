65033f399eb0fbc9283a0b82de315689782ca667
Zhongkai Liu <liu676785882@gmail.com>
2018-11-27 17:23:42 -0500

    Let backend to serve production app build

--- server/lib/app.js
+++ server/lib/app.js
@@ -2,15 +2,17 @@ require('rootpath')();
 const express = require('express');
 const cors = require('cors'); // allows cross origin resources sharing
 const bodyParser = require('body-parser'); // auto parse request body
-
+const path = require('path');
 const jwt = require('../helpers/jwt'); // javascrip web token functions
 const errorHandler = require('../helpers/error-handler');
 const dbs = require('../helpers/db');
 const cronjobs = require('../helpers/cronjob');
 
-const config = require('../config.json');
+const config = require('../config.js');
+const clientPath = path.resolve('client/build');
 
 const app = express();
+app.use(express.static(clientPath));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
@@ -25,9 +27,13 @@ app.use('/api/requests', require('../requests/request.controller'));
 // global error handler
 app.use(errorHandler);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientPath, 'index.html'));
+});
+
 // start server
 module.exports.start = function start() {
-  const port = process.env.NODE_ENV === 'production' ? 80 : (config.port || 3334);
+  const port = (config.port || 3334);
   const server = app.listen(port, function () {
     console.log('Connected to database: ' + config.db.uri);
     console.log('Server listening on port ' + port);
